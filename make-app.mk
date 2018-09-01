e5efe0d8cc0e3460a24528a081c62096384ac05f
Nikita Antonenkov <krigar1184@gmail.com>
2018-09-01 15:52:45 +0300

    Ansible environment config

--- make-app.mk
+++ make-app.mk
@@ -7,11 +7,14 @@ app-build:
 	docker-compose build
 
 app-bash:
-	docker-compose run --user=$(USER) app bash 
+	docker-compose run app bash 
 
-app-setup: app-build
+app-setup: development-setup-env app-build
 	docker-compose run app pip install -r requirements.txt
 	docker-compose run app pip install -r requirements.dev.txt
 
 app-test:
 	docker-compose run app python -m pytest
+
+development-setup-env:
+	ansible-playbook ansible/development.yml -i ansible/development -vv
--- services/app/run.py
+++ services/app/run.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python
+
+import os
 from danifier import app
+
+
+host = os.environ.get('FLASK_HOST')
+port = os.environ.get('FLASK_PORT')
+
+
+if __name__ == '__main__':
+    app.run(host=host, port=port, load_dotenv=True)
