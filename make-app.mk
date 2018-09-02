16e3f7d2a8211afed7f1ad7623e8c25f22ebd216
Nikita Antonenkov <krigar1184@gmail.com>
2018-09-02 13:44:09 +0300

    Fixes

--- make-app.mk
+++ make-app.mk
@@ -9,7 +9,7 @@ app-build:
 app-bash:
 	docker-compose run app bash 
 
-app-setup-development: development-setup-env app-build
+app-setup: development-setup-env app-build
 	docker-compose run app pip install -r requirements.txt
 	docker-compose run app pip install -r requirements.dev.txt
 
